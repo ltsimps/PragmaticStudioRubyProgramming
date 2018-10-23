5c3b630a73faac41f0ba03ad7c57dcf050ed5cdf
ltsimps <ltsimps@me.com>
2018-10-23 17:58:32 -0400

    Finished First 6 lessons

--- game_studio.rb
+++ game_studio.rb
@@ -1,8 +1,36 @@
-greeting  = "Hello ruby #{ 10 * 100 }"
+name1 = "Larry"
+name2 = "Curly"
+name4 = "Shamp"
+name3 = "moe"
 
-puts greeting
 
-health = 60
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
 
 
-puts "Larry's Health is #{health} testing "
+
+text = "#{name3.capitalize} has a health of #{health3}."
+puts text.center(50, '*')
+
+
+puts "#{name1.capitalize}'s' Health is #{health1} testing "
+puts "#{name2.upcase}'s' Health is #{health2} testing "
+
+
+health2 = health1
+puts "#{name2.upcase} has a health of #{health2}."
+
+
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+
+puts "#{name2.upcase} has a health of #{health2}."
+
+puts "#{name4.capitalize.ljust(30, '.')} has a health of #{health4}."
+
+
+
